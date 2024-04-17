e9311cfe24b01389b241cbf3a46ee90417fd49c5
Rafał Stottko <113773292+all2gos@users.noreply.github.com>
2024-04-17 11:29:57 +0200

    adding coulomb_diagonal param

--- params.py
+++ params.py
@@ -9,3 +9,5 @@ SAMPLE = 1 #the fraction of data from the dataset to be used for training
 
 SHUFFLE = True #enable data augmentation option by randomly selecting the order of coordinate atoms in a molecule
 CYCLE = 3 #number of images generated to one particle (should be greater than 1 only when coordinate shuffling is enabled)
+
+COULOMB_DIAGONAL = False # parameter specifying whether values on the diagonal for a coulomb matrix should be displayed
