f5c67187c6fe27a3d03c65aa5d22a2c6466befa4
Rafał Stottko <113773292+all2gos@users.noreply.github.com>
2024-04-22 15:02:08 +0200

    increase readability

--- scripts/params.py
+++ scripts/params.py
@@ -1,21 +1,24 @@
 import torch
 from torchvision import models 
 
-MATRIX_SIZE = 32
+#--- OVERALL PARAMETERS---#
 PATH = '' #your path to the working directory
-DB = 'qm7' #or qm7
 PREDICTED_VALUE = 'HOMO-LUMO Gap' #or 'Energy of HOMO', 'Energy of LUMO' or any other available properties
 
-TRAIN_DIR_NAME = 'train'
-TEST_DIR_NAME = 'test'
+#---DATABASE PARAMETERS---#
+DB = 'qm7' #name of your .csv database
 SAMPLE = 1 #the fraction of data from the dataset to be used for training
-
-SHUFFLE = True #enable data augmentation option by randomly selecting the order of coordinate atoms in a molecule
+SHUFFLE = True #enable data augmentation option by randomly selecting the order of coordinate atoms in a molecule, used only when creating a database and not images
 CYCLE = 3 #number of images generated to one particle (should be greater than 1 only when coordinate shuffling is enabled)
 
+#---IMAGE PARAMETERS---#
+
+MATRIX_SIZE = 32
+TRAIN_DIR_NAME = 'train'
+TEST_DIR_NAME = 'test'
 COULOMB_DIAGONAL = False # parameter specifying whether values on the diagonal for a coulomb matrix should be displayed
 
-#training parameters
+#---TRAINING PROCESS PARAMETERS---#
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 MODEL = models.resnet18(pretrained=False)
 LEARNING_RATE = 0.0003
