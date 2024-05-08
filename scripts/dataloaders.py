732168ca5ed9c8da06600f00158832d348ac3136
all2gos <stottkoraf@gmail.com>
2024-05-08 11:03:34 +0200

    refactoring

--- scripts/dataloaders.py
+++ scripts/dataloaders.py
@@ -9,40 +9,32 @@ from .params import *
 from .utils import making_rgb_numerically
 from torch.utils.data import DataLoader, TensorDataset
 
-
-
 def dataloader_ffn():
     raw_data = pd.read_csv(f'{PATH}/{DB}.csv')
 
-    def creating_images(start, end, bo, ds, split=0.1, step=1, verbose = False):
+    def creating_images(start, end, bo, ds, step=1):
         l = np.empty((0, 32*32*3 + 1))
         for chem in range(start, end+1, step):
             flatten = making_rgb_numerically(chem, bo, ds, scaling=False)
-            fl = np.vstack((flatten[0].reshape(32*32,1), flatten[1].reshape(32*32,1), flatten[2].reshape(32*32,1)))
+            fl = np.hstack(flatten).reshape(-1, 1)
             fl = np.append(fl, ds[PREDICTED_VALUE].iloc[chem])
             l = np.append(l, [fl], axis=0)
         return l
 
-    matrix = creating_images(0, len(raw_data)-1, bo, raw_data,  0.1, 1)
+    def create_dataloader(X, y, batch_size=BATCH_SIZE):
+        X_tensor = torch.tensor(X, dtype=torch.float32)
+        y_tensor = torch.tensor(y, dtype=torch.float32)
+        dataset = TensorDataset(X_tensor, y_tensor)
+        return DataLoader(dataset, batch_size=batch_size, shuffle=True)
 
-    training_volume = int(matrix.shape[1]*TRAIN_TEST_SPLIT)
+    matrix = creating_images(0, len(raw_data)-1, bo, raw_data)
 
-    X_train = matrix[:training_volume, :-1]
-    y_train = matrix[:training_volume, -1]
+    training_volume = int(len(matrix) * TRAIN_TEST_SPLIT)
 
-    X_tensor = torch.tensor(X_train, dtype=torch.float32)
-    y_tensor = torch.tensor(y_train, dtype=torch.float32)
+    X_train, y_train = matrix[:training_volume, :-1], matrix[:training_volume, -1]
+    X_test, y_test = matrix[training_volume:, :-1], matrix[training_volume:, -1]
 
-    dataset = TensorDataset(X_tensor, y_tensor)
-    train_loader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True)
-
-    X_test = matrix[training_volume:, :-1]
-    y_test = matrix[training_volume:, -1]
-
-    X_tensor = torch.tensor(X_test, dtype=torch.float32)
-    y_tensor = torch.tensor(y_test, dtype=torch.float32)
-
-    dataset = TensorDataset(X_tensor, y_tensor)
-    val_loader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True)
+    train_loader = create_dataloader(X_train, y_train)
+    val_loader = create_dataloader(X_test, y_test)
 
     return train_loader, val_loader
