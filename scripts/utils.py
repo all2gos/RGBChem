5e558c5784c3762947455ac0306821567f2107d7
all2gos <stottkoraf@gmail.com>
2024-05-09 11:48:05 +0200

    refactoring and adding p function

--- scripts/utils.py
+++ scripts/utils.py
@@ -6,9 +6,10 @@ from PIL import Image
 import random
 from pathlib import Path
 from .params import *
-import random
+from reax_ff_data import bo
 
 
+#does not work well, in effect making_df does not work well either
 try:
     files = os.listdir(f'{PATH}/data')
 except FileNotFoundError:
@@ -21,7 +22,7 @@ def extracting(f, shuffle = SHUFFLE):
     with open(f, 'r') as file:
         lines = file.readlines()
 
-    df_record = {'n_atoms':int(lines[0].strip())}
+    df_record = {'n_atoms': int(lines[0].strip()), 'atom_type': [], 'cords': [], 'mulliken': []}
 
     properties = lines[1].split("\t")
     
@@ -33,16 +34,11 @@ def extracting(f, shuffle = SHUFFLE):
     df_record.update(zip(labels, properties))
 
     #coordinates 
-    
-    cords = []
-    mulliken = []
-    atom_type = []
-
     for atom in range(df_record['n_atoms']):
-        temp = lines[2+atom].replace('\n','').split('\t')
-        atom_type.append(temp[0])
-        cords.append(temp[1:-1])
-        mulliken.append(temp[-1])
+        atom_record = lines[2+atom].replace('\n','').split('\t')
+        df_record['atom_type'].append(atom_record[0])
+        df_record['cords'].append(atom_record[1:-1])
+        df_record['mulliken'].append(atom_record[-1])
 
     '''If shuffle = True then the order of atoms in the molecule is randomized'''
     if shuffle == True:
@@ -61,7 +57,6 @@ def extracting(f, shuffle = SHUFFLE):
 def making_df(l=len(files), cycle=CYCLE):
     ''' Function using the extraction function to create an entire database from a list of .xyz file names'''
     df = []
-
     os.chdir(f'{PATH}/data')
 
     random_file = random.sample(files, l)
@@ -69,18 +64,22 @@ def making_df(l=len(files), cycle=CYCLE):
     for idx, file in enumerate(random_file):
         if idx % 1000 == 0:
             print(round(idx / l * 100, 2))
-        for c in range(cycle):
-            df.append(extracting(file))
-
+        df.extend(extracting(file) for _ in range(cycle))
         
     ds = pd.DataFrame(data = df)
-
     
     print(f'Database of lenght {len(ds)} was successfully created based on {len(files)} files')
 
     ds.to_csv(os.path.join(PATH, f"{DB}.csv"))
     return ds
 
+
+def scale_rgb_values(r, g, b, r_range=(0, 17.422), g_range=(0, 44.602), b_range=(0, 9)):
+    r = (255 * (r - r_range[0]) / (r_range[1] - r_range[0])).astype(int)
+    g = (255 * (g - g_range[0]) / (g_range[1] - g_range[0])).astype(int)
+    b = (255 * (b - b_range[0]) / (b_range[1] - b_range[0])).astype(int)
+
+    return r, g, b
 
 def making_rgb_numerically(row, bo, ds, scaling=True, verbose = False):
     '''function that, based on a single row in the database, creates three 
@@ -97,37 +96,20 @@ def making_rgb_numerically(row, bo, ds, scaling=True, verbose = False):
     g = coulomb_matrix(cords, n_atoms, atom_types, diagonal = False)
     #b = (atomic_charge(atom_types, n_atoms))
     b = (bond_order(distance(cords, n_atoms), atom_types, bo))
+    return scale_rgb_values(r,g,b) if scaling ==True else r,g,b
 
-
-    #output calibration
-    if scaling == True:
-        r_max= 17.422
-        r_min= 0.0
-        g_max= 44.60282803468642
-        g_min= 0.0
-        b_max= 9.0
-        b_min= 0.0
-
-
-
-        r = (255*(r - r_min) / (r_max - r_min)).astype(int)
-        g = (255*(g - g_min) / (g_max - g_min)).astype(int)
-        b = (255*(b - b_min) / (b_max - b_min)).astype(int)
-    return r,g,b
-
-def calibration(ds, d, pbo, r0):
+def calibration(ds, d, bo):
 
     '''A function that checks whether the matrices generated 
     on the entered settings contain values in the range 0-255
     '''
-
     data = []
     c = []
     start = random.randint(0,5) #to avoid too long execution
     for compound in range(start, len(ds), int(len(ds)/d)): #step != 1 to avoid too long execution
 
         try:
-            data.append(making_rgb_numerically(compound,pbo, r0))
+            data.append(making_rgb_numerically(compound, bo))
         except ValueError:
             print(ds.ID.iloc[compound])
         c.append(ds.ID.iloc[compound])
@@ -144,17 +126,14 @@ def calibration(ds, d, pbo, r0):
       max_values.append(max_group)
       min_values.append(min_group)
 
-    print("r_max=", max_values[0])
-    print("r_min=", min_values[0])
-    print("g_max=", max_values[1])
-    print("g_min=", min_values[1])
-    print("b_max=", max_values[2])
-    print("b_min=", min_values[2])
-
+    print("r_range=", (min_values[0], max_values[1]))
+    print("g_range=", (min_values[1], max_values[1]))
+    print("b_range=", (min_values[2], max_values[2]))
 
 def making_rgb(mat, id, label):
   ''' Function that takes the result of the making_rgb_numerically function and transforms the set of three matrices into a finished png image'''
-  combined = np.transpose(np.array((mat[0],mat[1],mat[2])),(1,2,0))
+  
+  combined = np.transpose(np.array((mat[0][0],mat[0][1],mat[0][2])),(1,2,0))
 
   img = np.array(combined, dtype=np.uint8)
 
@@ -162,21 +141,41 @@ def making_rgb(mat, id, label):
 
   pImg.save(f"{PATH}/{label}/{id}.png")
 
-def creating_images(start, end, bo, split=0.1, step=1):
+def creating_images(start, end, bo, ds, split=0.1, step=1):
     ''' A function that is an automation of the making_rgb function '''
     os.makedirs(f'{PATH}/{TRAIN_DIR_NAME}', exist_ok=True)
     os.makedirs(f'{PATH}/{TEST_DIR_NAME}', exist_ok = True)
 
     for chem in range(start, end+1, step):
-
         if random.randint(1,int(1/split)) == int(1/split):
-            making_rgb(making_rgb_numerically(chem, pbo, r0), ds.ID.iloc[chem], label = TEST_DIR_NAME)
+            making_rgb(making_rgb_numerically(chem, bo, ds), ds.ID.iloc[chem], label = TEST_DIR_NAME)
             print(f"{chem} goes to test set")
         else:
 
-            making_rgb(making_rgb_numerically(chem, pbo, r0), ds.ID.iloc[chem], label=TRAIN_DIR_NAME)
+            making_rgb(making_rgb_numerically(chem, bo, ds), ds.ID.iloc[chem], label=TRAIN_DIR_NAME)
 
+import matplotlib.pyplot as plt
+def p(num_epochs, losses, accuracies):
+  ''' Function to plot change in loss and accuracy values while training the model '''
+  x = range(num_epochs)[2:]
 
+  fig, ax1 = plt.subplots()
+
+  losses = [1/x for x in losses]
+  color = 'tab:blue'
+  ax1.set_xlabel('Epochs')
+  ax1.set_ylabel('loss', color=color)
+  ax1.plot(x, losses[2:], color=color)
+  ax1.tick_params(axis='y', labelcolor=color)
+
+  ax2 = ax1.twinx()  
+  color = 'tab:red'
+  ax2.set_ylabel('accuracy', color=color)
+  ax2.plot(x, accuracies[2:], color=color)
+  ax2.tick_params(axis='y', labelcolor=color)
+
+  plt.title('Loss and accuracy[meV] value')
+  plt.show()
     
 if __name__ == '__main__':
 
@@ -201,7 +200,6 @@ if __name__ == '__main__':
         s = time.time()
         ds = making_df(d)
         ds[PREDICTED_VALUE] *= 27211
-
         
         e = time.time()
         print(f'Creating dataframe took {round(e-s,2)} seconds')
@@ -214,63 +212,13 @@ if __name__ == '__main__':
             lines = f.readlines()
             lines = [x.strip().split() for x in lines]
 
-        pbo = lines[168:219]
-        pbo = pd.DataFrame(data=pbo).drop([30,38]).reset_index(drop=True).drop([1,3],axis=1)
-        pbo.columns = ['atom1','atom2','pbo1','pbo2','pbo3','pbo4','pbo5','pbo6']
-
-        rij = lines[320:353]
-        rij = pd.DataFrame(data=rij).drop([1,3,4,5,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25],axis=1)
-        rij.columns = ['atom1','atom2','rsigma','rpi','rpipi']
-
-
-        rii = lines[42:53]
-        rii = pd.DataFrame(data = rii).drop([1], axis=1)
-        rii.columns = ['atom1','rsigma','rpi','rpipi']
-        rii = rii.assign(atom2 = rii.atom1)
-
-        rii = rii[['atom1','atom2','rsigma','rpi','rpipi']]
-
-        r0 = pd.concat([rij, rii]).reset_index(drop=True)
-        calibration(ds, d, pbo, r0)
+        calibration(ds, d, bo)
         e = time.time()
         print(f"Calibration took {round(e-s,2)} seconds")
 
     if args.create_files:
         s = time.time()
-        bo = {'CC':[-0.075,6.8316,-0.1,9.2605,-0.4591,37.7369,1.3825,1.1359,1.2104],
-            'OO':[-0.1302,6.2919,-0.1239,7.6487,-0.1244,29.6439,1.2477,1.0863,0.9088],
-            'CF': [-0.0953,5.7786,-0.25,15,-0.5,35,1.6,1.3144,-1],
-            'OF':[-0.1,10,-0.25,15,-0.5,45,-1,-1,-1],
-            'FF':[-0.1442,5.2741,-0.25,15,-0.35,25,1.162,-0.1,-1],
-            'CN':[-0.115,5.8717,-0.2103,7.4487,-0.2709,29.9009,1.9263,1.4778,1.1446],
-            'ON':[-0.1937,5.214,-0.2175,7.0255,-0.4462,34.9336,1.9531,1.3018,1.0984],
-            'FN':[-0.1,10,-0.25,15,-0.5,45,-1,-1,-1],
-            'NN':[-0.1791,5.8008,-0.205,9.7308,-0.1,19.085,1.6157,1.2558,1.0439],
-            'CH':[-0.05,6.8315,0,0,0,6,1.4,1.1203,-1],
-            'HH':[-0.0593,4.8358,0,0,0,6,0.7853,-0.1,-0.1],
-            'CO':[-0.1463,5.2913,-0.3174,7.0303,-0.1613,10.8851,1.8523,1.2775,1.1342],
-            'HO':[-0.0657,5.0451,0,0,0,6,1.68,0.9013,-1],
-            'HN':[-0.0395,7.2218,0,0,0,6,2.3,1.0156,-1],
-            'HF':[-0.0794,6.1403,-0.2,15,-0.2,16,-1,-1,-1],
-            'FC': [-0.0953,5.7786,-0.25,15,-0.5,35,1.6,1.3144,-1],
-            'FO':[-0.1,10,-0.25,15,-0.5,45,-1,-1,-1],
-            'NC':[-0.115,5.8717,-0.2103,7.4487,-0.2709,29.9009,1.9263,1.4778,1.1446],
-            'NO':[-0.1937,5.214,-0.2175,7.0255,-0.4462,34.9336,1.9531,1.3018,1.0984],
-            'NF':[-0.1,10,-0.25,15,-0.5,45,-1,-1,-1],
-            'HC':[-0.05,6.8315,0,0,0,6,1.4,1.1203,-1],
-            'OC':[-0.1463,5.2913,-0.3174,7.0303,-0.1613,10.8851,1.8523,1.2775,1.1342],
-            'OH':[-0.0657,5.0451,0,0,0,6,1.68,0.9013,-1],
-            'NH':[-0.0395,7.2218,0,0,0,6,2.3,1.0156,-1],
-            'FH':[-0.0794,6.1403,-0.2,15,-0.2,16,-1,-1,-1]}
-
         creating_images(start, end, bo, 0.1, 1)
         e = time.time()
         print(f"Creation of images took {round(e-s,2)} seconds")
-
-
-
-    
-
-
-
 
