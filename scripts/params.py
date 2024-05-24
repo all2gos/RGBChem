1efcf4f54d4a69d29a54d7f461ea8ca187c9d983
all2gos <stottkoraf@gmail.com>
2024-05-24 14:36:38 +0200

    Adding LOG_FILE param

--- scripts/params.py
+++ scripts/params.py
@@ -3,13 +3,13 @@ from torchvision import models
 
 #--- OVERALL PARAMETERS---#
 PATH = '.' #your path to the working directory
-PREDICTED_VALUE = 'HOMO-LUMO Gap' #or 'Energy of HOMO', 'Energy of LUMO' or any other available properties
+PREDICTED_VALUE = 'bandgap_correct' #or 'Energy of HOMO', 'Energy of LUMO' or any other available properties
 
 #---DATABASE PARAMETERS---#
-DB = 'qm7_demo' #name of your .csv database
+DB = 'qm9' #name of your .csv database
 SAMPLE = 1 #the fraction of data from the dataset to be used for training
-SHUFFLE = True #enable data augmentation option by randomly selecting the order of coordinate atoms in a molecule, used only when creating a database and not images
-CYCLE = 3 #number of images generated to one particle (should be greater than 1 only when coordinate shuffling is enabled)
+SHUFFLE = False #enable data augmentation option by randomly selecting the order of coordinate atoms in a molecule, used only when creating a database and not images
+CYCLE = 1 #number of images generated to one particle (should be greater than 1 only when coordinate shuffling is enabled)
 
 #---IMAGE PARAMETERS---#
 
@@ -25,3 +25,10 @@ MOMENTUM = 0.8
 EPOCHS = 64
 TRAIN_TEST_SPLIT = 0.8 
 BATCH_SIZE = 32
+
+#---LOGGING---#
+#name of the log file
+if SHUFFLE == False:
+    LOG_FILE = f'{DB}_e{EPOCHS}_{PREDICTED_VALUE}_bs{BATCH_SIZE}.log' 
+else: 
+    LOG_FILE = f'{DB}_e{EPOCHS}_{PREDICTED_VALUE}_bs{BATCH_SIZE}_shuffle_{CYCLE}.log' 
