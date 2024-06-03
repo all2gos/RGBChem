a804374cddbc13f516bdebe40ac00f243eea71b3
all2gos <stottkoraf@gmail.com>
2024-06-03 15:58:45 +0200

    dealing with CUDA memory

--- learner.py
+++ learner.py
@@ -43,7 +43,7 @@ def learner(dl, model):
             epoch_loss = running_loss / len(dl[0].dataset)
 
             original_value= torch.cat([x[1] for x in dl[1]]) #in the form of one dimensional tensor
-            predicted_value = torch.cat([model(x[0]) for x in dl[1]]).t().to(DEVICE)
+            predicted_value = torch.cat([model(x[0].to(DEVICE)) for x in dl[1]]).t()
 
             acc = sum(sum(torch.abs(original_value-predicted_value)))/predicted_value.size()[1]*27211
             
