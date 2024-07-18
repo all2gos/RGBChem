17e194d6f5552d2dec1c9add8550fcf2793d2ca9
all2gos <stottkoraf@gmail.com>
2024-07-18 10:31:47 +0200

    add info  about shuffling type into log name

--- scripts/params.py
+++ scripts/params.py
@@ -11,7 +11,7 @@ PREDICTED_VALUE = 'bandgap' #or 'Energy of HOMO', 'Energy of LUMO' or any other 
 #---DATABASE PARAMETERS---#
 DB = 'qm7_demo' #name of your .csv database
 SAMPLE = 1 #the fraction of data from the dataset to be used for training
-CYCLE = 1 #number of images generated to one particle (should be greater than 1 only when coordinate shuffling is enabled)
+CYCLE = 256 #number of images generated to one particle (should be greater than 1 only when coordinate shuffling is enabled)
 SHUFFLE = 'partial' #shuffle is a complex parameter: when it is set to full then each time order of the atoms in particle is randomly selected,
 #when it is set to partial then the algorithm split atoms in particle into two groups: heavy atoms and hydrogens, and randomly selected order 
 #inside these particular group
@@ -19,7 +19,7 @@ SHUFFLE = 'partial' #shuffle is a complex parameter: when it is set to full then
 
 
 #---IMAGE PARAMETERS---#
-MATRIX_SIZE = 28 #if zero then images doee not have margins
+MATRIX_SIZE = 0 #if zero then images doee not have margins
 TRAIN_DIR_NAME = 'train'
 TEST_DIR_NAME = 'test'
 TYPE_OF_IMAGE = 'A' #or any other letter available 
@@ -43,7 +43,7 @@ DELTA = 0
 
 #---LOGGING---#
 #name of the log file
-if SHUFFLE == False:
+if SHUFFLE not in ['partial','full']:
     LOG_FILE = f'{DB}{TYPE_OF_IMAGE}{MODEL}_{MARGIN}_e{EPOCHS}_{PREDICTED_VALUE}_bs{BATCH_SIZE}.log' 
 else: 
-    LOG_FILE = f'{DB}{TYPE_OF_IMAGE}{MODEL}_{MARGIN}_e{EPOCHS}_{PREDICTED_VALUE}_bs{BATCH_SIZE}_shuffle_{CYCLE}.log' 
+    LOG_FILE = f'{DB}{TYPE_OF_IMAGE}{MODEL}_{MARGIN}_e{EPOCHS}_{PREDICTED_VALUE}_bs{BATCH_SIZE}_shuffle_{SHUFFLE}_{CYCLE}.log' 
