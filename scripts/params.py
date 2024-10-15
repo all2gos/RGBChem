2cf268e9acc477847e0ff9b303709b5eebea42b5
all2gos <stottkoraf@gmail.com>
2024-10-15 15:15:07 +0200

    add 3 parameters

--- scripts/params.py
+++ scripts/params.py
@@ -1,18 +1,17 @@
 import torch
 from torchvision import models 
 
-__all__ = ['BATCH_SIZE', 'CYCLE', 'DB', 'DEVICE', 'EPOCHS', 'LEARNING_RATE', 'LOG_FILE', 'MATRIX_SIZE', 'MOMENTUM', 'PATH', 'PREDICTED_VALUE', 'SAMPLE', 'SHUFFLE', 'TEST_DIR_NAME',
- 'TRAIN_DIR_NAME', 'TRAIN_TEST_SPLIT', 'SCALING', 'DELETE','TYPE_OF_IMAGE', 'PATIENCE', 'DELTA','MODEL','RANDOM_OR','MARGIN','RESIZE', 'COMPRESSION','STEP']
+__all__ = ['BATCH_SIZE', 'CYCLE', 'DB', 'DEVICE', 'EPOCHS', 'LEARNING_RATE', 'LOG_FILE', 'MATRIX_SIZE', 'MOMENTUM', 'PATH', 'PREDICTED_VALUE', 'SAMPLE', 'SHUFFLE', 'TEST_DIR_NAME','TRAIN_DIR_NAME', 'TRAIN_TEST_SPLIT', 'SCALING', 'DELETE','TYPE_OF_IMAGE', 'PATIENCE', 'DELTA','MODEL','RANDOM_OR','MARGIN','RESIZE', 'COMPRESSION','STEP','BATTERY_LEVEL_CONTROL','MULTIPROCESS','NUM_PROC']
 
 #--- OVERALL PARAMETERS---#
-PATH = '.' #your path to the working directory
+PATH = '/home/rstottko/RGBChem' #your path to the working directory
 PREDICTED_VALUE = 'bandgap' #or 'Energy of HOMO', 'Energy of LUMO' or any other available properties
 
 #---DATABASE PARAMETERS---#
-DB = 'qm9'
+DB = 'qm9_3'
 SAMPLE = 1 #the fraction of data from the dataset to be used for training
-CYCLE = 1
-SHUFFLE = 'none' #none, full, partial, groups
+CYCLE = 2
+SHUFFLE = 'none'
 #none = defined order of atoms
 #full = completely random order
 #partial = divide atoms into two groups (H and not H) and randomly selection in these groups
@@ -23,26 +22,30 @@ COMPRESSION = False #if True then all dataset is smaller
 MATRIX_SIZE = 0 #if zero then images doee not have margins
 TRAIN_DIR_NAME = 'train'
 TEST_DIR_NAME = 'test'
-TYPE_OF_IMAGE = 'H'
+TYPE_OF_IMAGE = 'A'
 RANDOM_OR = False #if True then images will be placed randomly
 SCALING = True
 DELETE = True  #if True then the script will delete all so far generated files and created new one from scratch
 MARGIN = 'avg' #the way the margins are filled
 RESIZE = 32
-STEP = 60 #calibration parameter
+STEP = 30 #calibration parameter
 #---TRAINING PROCESS PARAMETERS---#
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 MODEL = 'resnet18'
 LEARNING_RATE = '0.0003'
 MOMENTUM = 0.9
-EPOCHS = 512
+EPOCHS = 2048
 TRAIN_TEST_SPLIT = 0.9
-BATCH_SIZE = 256
+BATCH_SIZE = 64
 
 #---EARLY STOPPING---#
 PATIENCE = 20
 DELTA = 0
 
+#---OTHERS---#
+BATTERY_LEVEL_CONTROL = False #if True then script checks the battery charge level after each epoch and wait 
+MULTIPROCESS = True
+NUM_PROC=8
 #---LOGGING---#
 #name of the log file
 if SHUFFLE not in ['partial','full']:
