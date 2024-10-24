615b4dc138f385d7a0b1a9c3b5641c265bfb54d7
all2gos <stottkoraf@gmail.com>
2024-10-24 15:40:19 +0200

    add some printing

--- scripts/qm_vanilla_handling.py
+++ scripts/qm_vanilla_handling.py
@@ -6,9 +6,12 @@ from scripts.utils import modify_params
 
 def create_qm_vanilla_file():
     if f"{DB.split('_')[0]}_vanilla.csv" not in os.listdir({PATH}):
+        print(f"Creating qm_vanilla.csv file")
         modify_param({'CYCLE':1})
         making_df()
         os.system(f"mv {PATH}/{DB} {PATH}/{DB.split('_')[0]}_vanilla.csv")
+    else:
+        print(f"{DB.split('_')[0]}_vanilla.csv have been found in directory")
 
 
 
