6fab61511c2c0c8020a4f3840839d8be16ae3ead
all2gos <stottkoraf@gmail.com>
2024-10-24 14:06:23 +0200

    change the workflow: instead of predefined list of params now we get random set

--- automate_workflow.py
+++ automate_workflow.py
@@ -1,8 +1,9 @@
-import os
-import re
+import os, random
+import re, shutil
 import datetime
 import importlib
 import scripts.params as params_module 
+from scripts.params import PATH
 
 
 def modify_params(changes):
@@ -25,50 +26,61 @@ def remove_csv(file_path):
     else:
         print(f'{file_path} does not exist.')
 
+def remove_dir(directory):
+    if os.path.isdir(directory):
+        shutil.rmtree(directory)
+        print(f"Directory {directory} have been removed.")
+        
 # 3. Uruchomienie pliku run_fastai.py
 def run_script():
     os.system(f'nohup python run_fastai.py > {datetime.datetime.now().strftime("%H_%M_%d_%m_%Y")}.log')
 
+def get_random_params():
+    DB = random.choice(['qm9','qm9_2','qm9_3','qm9_4','qm9_6'])
+    BATCH_SIZE = random.choice([x for x in range(16,100,4)])
+    CYCLE = random.choice([1,2,4,6,8,12]) if DB != 'qm9' else random.choice([1,2,4,6])
+    TYPE_OF_IMAGE = random.choice(['A','B','C','D','E'])
+    SHUFFLE = random.choice(['full','none','partial','groups'])
+    MOMENTUM = round(random.uniform(0.8,0.9),3)
+    LEARNING_RATE = random.choice(['0.0003','0.001','0.0005','0.0008'])
 
+    if DB not in ['qm9','qm9_2']:
+        MODEL = random.choice(['resnet18','resnet34','resnet50','vgg16_bn', 'vgg19_bn','densenet121','squeezenet1_0', 'squeezenet1_1'])
+    else:
+        MODEL = random.choice(['resnet18','resnet34','vgg16_bn', 'vgg19_bn','densenet121','squeezenet1_0', 'squeezenet1_1'])
+
+
+    RESIZE_BOOL = random.choices([0, 1], weights=[4, 1])[0]
+
+    if RESIZE_BOOL:
+        RESIZE = random.choice([32,36,40,44,48])
+        MATRIX_SIZE = 0
+        RANDOM_OR = False
+        
+    else:
+        MATRIX_SIZE = random.choice([32,36,40,44,48])
+        RANDOM_OR = random.choice([False, True])
+        MARGIN = random.choice(['avg','black'])
+        RESIZE = 0
+
+    return {'DB':DB,'BATCH_SIZE':BATCH_SIZE,'CYCLE':CYCLE,'TYPE_OF_IMAGE':TYPE_OF_IMAGE,'SHUFFLE':SHUFFLE,'MOMENTUM':MOMENTUM,
+           'LEARNING_RATE':LEARNING_RATE,'MODEL':MODEL,'RESIZE':RESIZE,'MATRIX_SIZE':MATRIX_SIZE,'RANDOM_OR':RANDOM_OR,'MARGIN':MARGIN}
 # Przykład użycia:
 if __name__ == "__main__":
  
-    #cycle shuffle i base
+   for _ in range(20):
+        os.chdir(PATH)
+        modify_params(get_random_params())
+        importlib.reload(params_module)
+        csv_file = f'{params_module.DB}.csv'
+        remove_csv(csv_file)
+        remove_dir('data')
+        remove_dir('test')
+        remove_dir('train')
+        run_script()
 
-    #{'DB': 'qm7_demo', 'BATCH_SIZE': 16, 'CYCLE': 1, 'TYPE_OF_IMAGE': 'A'},
-    #{'DB': 'qm8_demo', 'BATCH_SIZE': 32, 'CYCLE': 1, 'TYPE_OF_IMAGE': 'B'},
-    #{'DB': 'qm9_32', 'BATCH_SIZE': 24, 'CYCLE': 1, 'TYPE_OF_IMAGE': 'H'},
-    #{'DB': 'qm9_6', 'BATCH_SIZE': 48, 'CYCLE': 1, 'TYPE_OF_IMAGE': 'G'},
-        
-    trainings = [
-    #{'DB': 'qm9_3', 'BATCH_SIZE': 96, 'CYCLE': 1, 'TYPE_OF_IMAGE': 'E'},
-    #{'DB': 'qm9_2', 'BATCH_SIZE': 64, 'CYCLE': 1, 'TYPE_OF_IMAGE': 'D'},
-    #{'DB': 'qm9', 'BATCH_SIZE': 128, 'CYCLE': 1, 'TYPE_OF_IMAGE': 'C'},
-    #{'DB': 'qm7_demo', 'BATCH_SIZE': 24, 'CYCLE': 2, 'TYPE_OF_IMAGE': 'B'},
-    #{'DB': 'qm8_demo', 'BATCH_SIZE': 48, 'CYCLE': 2, 'TYPE_OF_IMAGE': 'A'},
-    #{'DB': 'qm9_32', 'BATCH_SIZE': 16, 'CYCLE': 2, 'TYPE_OF_IMAGE': 'G'},
-    #{'DB': 'qm9_6', 'BATCH_SIZE': 32, 'CYCLE': 2, 'TYPE_OF_IMAGE': 'H'},
-    #{'DB': 'qm9_4', 'BATCH_SIZE': 128, 'CYCLE': 2, 'TYPE_OF_IMAGE': 'E'},
-    #{'DB': 'qm9_3', 'BATCH_SIZE': 64, 'CYCLE': 2, 'TYPE_OF_IMAGE': 'F'},
-    #{'DB': 'qm9_2', 'BATCH_SIZE': 96, 'CYCLE': 2, 'TYPE_OF_IMAGE': 'C'},
-    #{'DB': 'qm9', 'BATCH_SIZE': 256, 'CYCLE': 2, 'TYPE_OF_IMAGE': 'D'},
-    #{'DB': 'qm7_demo', 'BATCH_SIZE': 32, 'CYCLE': 4, 'TYPE_OF_IMAGE': 'C'},
-    #{'DB': 'qm8_demo', 'BATCH_SIZE': 16, 'CYCLE': 4, 'TYPE_OF_IMAGE': 'D'},
-    #{'DB': 'qm9_32', 'BATCH_SIZE': 48, 'CYCLE': 4, 'TYPE_OF_IMAGE': 'F'},
-    #{'DB': 'qm9_6', 'BATCH_SIZE': 24, 'CYCLE': 4, 'TYPE_OF_IMAGE': 'E'},
-    #{'DB': 'qm9_4', 'BATCH_SIZE': 96, 'CYCLE': 4, 'TYPE_OF_IMAGE': 'H'},
-    #{'DB': 'qm9_3', 'BATCH_SIZE': 256, 'CYCLE': 4, 'TYPE_OF_IMAGE': 'G'},
-    #{'DB': 'qm9_2', 'BATCH_SIZE': 128, 'CYCLE': 4, 'TYPE_OF_IMAGE': 'B'},
-    #{'DB': 'qm9', 'BATCH_SIZE': 64, 'CYCLE': 4, 'TYPE_OF_IMAGE': 'A'},
-    #{'DB': 'qm7_demo', 'BATCH_SIZE': 48, 'CYCLE': 8, 'TYPE_OF_IMAGE': 'D'},
-    #{'DB': 'qm8_demo', 'BATCH_SIZE': 24, 'CYCLE': 8, 'TYPE_OF_IMAGE': 'C'},
-    #{'DB': 'qm9_32', 'BATCH_SIZE': 32, 'CYCLE': 8, 'TYPE_OF_IMAGE': 'E'},
-    #{'DB': 'qm9_4', 'BATCH_SIZE': 64, 'CYCLE': 8, 'TYPE_OF_IMAGE': 'G'},
-    #{'DB': 'qm9_3', 'BATCH_SIZE': 128, 'CYCLE': 8, 'TYPE_OF_IMAGE': 'H'},
-    #{'DB': 'qm9_2', 'BATCH_SIZE': 256, 'CYCLE': 8, 'TYPE_OF_IMAGE': 'A'},
-    #{'DB': 'qm7_demo', 'BATCH_SIZE': 64, 'CYCLE': 16, 'TYPE_OF_IMAGE': 'E'},
-    #{'DB': 'qm8_demo', 'BATCH_SIZE': 128, 'CYCLE': 16, 'TYPE_OF_IMAGE': 'F'},
-    #{'DB': 'qm7_demo', 'BATCH_SIZE': 96, 'CYCLE': 32, 'TYPE_OF_IMAGE': 'F'},
+'''
+
     {'DB': 'qm9_32', 'BATCH_SIZE': 96, 'CYCLE': 16, 'TYPE_OF_IMAGE': 'D'},
     {'DB': 'qm8_demo', 'BATCH_SIZE': 256, 'CYCLE': 32, 'TYPE_OF_IMAGE': 'E'},
     {'DB': 'qm7_demo', 'BATCH_SIZE': 128, 'CYCLE': 64, 'TYPE_OF_IMAGE': 'G'},
@@ -112,14 +124,8 @@ if __name__ == "__main__":
     {'DB': 'qm9', 'BATCH_SIZE': 32, 'CYCLE': 16, 'TYPE_OF_IMAGE': 'A'},
 
     {'DB': 'qm9_4', 'BATCH_SIZE': 256, 'CYCLE': 1, 'TYPE_OF_IMAGE': 'F'}] #batch_size spory
-
+'''
     
 
-    for training in trainings:
-        modify_params(training)
-        importlib.reload(params_module)
-        csv_file = f'{params_module.DB}.csv'
-        remove_csv(csv_file)
-        run_script()
 
     
