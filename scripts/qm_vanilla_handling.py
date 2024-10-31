32ede37846089069c56ca93adc8694195f4f8119
all2gos <stottkoraf@gmail.com>
2024-10-31 09:44:02 +0100

    minor bug fix

--- scripts/qm_vanilla_handling.py
+++ scripts/qm_vanilla_handling.py
@@ -5,7 +5,7 @@ from scripts.params import *
 from scripts.utils import modify_params 
 
 def create_qm_vanilla_file():
-    if f"{DB.split('_')[0]}_vanilla.csv" not in os.listdir({PATH}):
+    if f"{DB.split('_')[0]}_vanilla.csv" not in list(os.listdir(PATH)):
         print(f"Creating qm_vanilla.csv file")
         modify_param({'CYCLE':1})
         making_df()
