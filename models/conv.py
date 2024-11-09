2dfee791809137163134b8a10c2f636c5ddbec30
all2gos <stottkoraf@gmail.com>
2024-11-09 19:37:10 +0100

    create S1CNN and S2CNN architecture

--- models/conv.py
+++ models/conv.py
@@ -1,11 +1,14 @@
 import torch
 import torch.nn as nn
+from scripts.params import *
 
-class SCNN(nn.Module):
+SIZE_OF_IMG = max(MATRIX_SIZE, RESIZE)
+
+class S1CNN(nn.Module):
     def __init__(self):
-        super(SCNN,self).__init__()
+        super(S1CNN,self).__init__()
         self.c1 = nn.Conv2d(in_channels=3,out_channels=6,kernel_size=5,stride=1,padding=2)
-        self.fc1 = nn.Linear(6*16*16,8*16)
+        self.fc1 = nn.Linear(6*int(SIZE_OF_IMG/2)*int(SIZE_OF_IMG/2),8*16)
         self.fc2 = nn.Linear(8*16, 32)
         self.fc3 = nn.Linear(32, 1)
 
@@ -25,21 +28,31 @@ class SCNN(nn.Module):
         x = self.fc3(x)
         return x
     
-
-class SimpleCNN(nn.Module):
+class S2CNN(nn.Module):
     def __init__(self):
-        super(SimpleCNN, self).__init__()
-        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1)
-        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1)
-        self.fc1 = nn.Linear(32 * 7 * 7, 128)
-        self.fc2 = nn.Linear(128, 10)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.relu = nn.ReLU()
+        super(S2CNN, self).__init__()
+        
+        self.c1 = nn.Conv2d(in_channels=3, out_channels=6, kernel_size=5, stride=1, padding=2)
+        self.c2 = nn.Conv2d(in_channels=6, out_channels=12, kernel_size=5, stride=1, padding=2)
+        
+        self.fc1 = nn.Linear(12 * int(SIZE_OF_IMG / 4) * int(SIZE_OF_IMG / 4), 8 * 16)
+        self.fc2 = nn.Linear(8 * 16, 32)
+        self.fc3 = nn.Linear(32, 1)
 
-    def forward(self, x):
-        x = self.pool(self.relu(self.conv1(x)))
-        x = self.pool(self.relu(self.conv2(x)))
-        x = x.view(-1, 32 * 7 * 7)
+        self.max_pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(p=0.2)
+
+    def forward(self, img):
+        x = self.c1(img)
+        x = self.relu(self.max_pool(x))
+        x = self.c2(x)
+        x = self.relu(self.max_pool(x))
+        x = torch.flatten(x, 1)
         x = self.relu(self.fc1(x))
-        x = self.fc2(x)
+        x = self.dropout(x)
+        x = self.relu(self.fc2(x))
+        x = self.dropout(x)
+        x = self.fc3(x)
+        
         return x
