1f21c1589ad1c1734722cbc01320372b912c097e
all2gos <stottkoraf@gmail.com>
2025-03-27 12:40:59 +0100

    building training loop on plain pytorch

--- run_learning.py
+++ run_learning.py
@@ -1,26 +1,139 @@
-import torch.optim as optim
-import matplotlib.pyplot as plt
-import torch.nn as nn
-import random
-import torch
 import pandas as pd
+from scripts.dataloaders import dataloader_conv
+from scripts.params import *
+from scripts.params import *
+from scripts.qm_vanilla_handling import create_qm_vanilla_file
+import torch
+import time
+import subprocess
+import os
+import random
+import torch.optim as optim
+import torch.nn as nn
 import numpy as np
 
-from scripts.params import *
-from models.flatten import SimpleNN
-from models.conv import SCNN
+from models.conv import *
 
-from scripts.dataloaders import dataloader_conv, dataloader_ffn
-from learner import learner
+def move_one_image_to_the_outside():
+    '''Function that after training procedure choose one .png and move it to the external file'''
+
+    img_list= os.listdir(f"{PATH}/train")
+    img_name = img_list[random.randint(1,len(img_list))]
+
+    os.system(f"mv {PATH}/train/{img_name} {PATH}")
+    print(f'File {img_name} have been successfully moved to {PATH}')
+
+def get_list(path):
+    l = []
+    for plik in os.listdir(path):
+        if os.path.isfile(os.path.join(path, plik)):
+            l.append(plik)
+    return l
+
+def mae(y_true, y_pred):
+    return np.mean(np.abs(y_true - y_pred))
+
+#load data points
+train_loader, val_loader, test_loader, raw_data = dataloader_conv()
+ds = pd.read_csv(f'{PATH}/{DB}.csv')
+
+from scripts.params import __all__
+print(f"All params list")
+for name in __all__:
+    print(f"{name} = {globals()[name]}")
+
+path = f'{PATH}/{TRAIN_DIR_NAME}'
+get_image_files = get_list(path)
+
+f = os.listdir(f"{PATH}/{TRAIN_DIR_NAME}")
+f = [x[:-4] for x in f]
+
+filtered = ds[ds.ID.isin(f)]
+print(f'{len(filtered)} out of {len(ds)} samples were selected')
 
 
-dl = dataloader_conv()
-#model = SCNN()
+#load model
+if MODEL in ['S1CNN()','S2CNN()']:
+    model = eval(MODEL)
 
-from torchvision.models  import resnet18, ResNet18_Weights
-model = resnet18(weights=ResNet18_Weights.DEFAULT).to(DEVICE)
-num_ftrs = model.fc.in_features
-model.fc = nn.Linear(num_ftrs, 1)
 
-#model = SCNN()
-m = learner(dl, model)
+#gpu migration
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model.to(device)
+
+#optimizer and loss function
+optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
+criterion = nn.L1Loss()
+
+#training step
+def train_one_epoch(model, train_loader, optimizer, criterion, device):
+    model.train()
+    running_loss = 0.0
+    for inputs, targets in train_loader:
+        inputs, targets = inputs.to(device), targets.to(device)
+        
+        optimizer.zero_grad()
+        
+        outputs = model(inputs).squeeze()
+        #print(outputs.size(), targets.size())
+        loss = criterion(outputs,  targets)
+        
+        loss.backward()
+        optimizer.step()
+        
+        running_loss += loss.item()
+    
+    return running_loss / len(train_loader)
+
+#validation step
+def validate(model, valid_loader, criterion, device):
+    model.eval()
+    running_loss = 0.0
+    predictions, targets = [], []
+    
+    with torch.no_grad():
+        for inputs, targets in valid_loader:
+            inputs, targets = inputs.to(device), targets.to(device)
+            #print(inputs.size(), targets.size())
+            #print(inputs)
+            outputs = model(inputs).squeeze()
+            loss = criterion(outputs, targets)
+            running_loss += loss.item()
+            
+        outputs, targets = outputs.cpu().numpy(), targets.cpu().numpy()
+        print(outputs[:4])
+
+    return running_loss / len(valid_loader), mae(np.array(targets), np.array(outputs)), np.var(np.array(outputs))/np.var(np.array(targets))
+
+
+best_valid_loss = float('inf')
+patience_counter = 0
+early_stopping_patience = PATIENCE
+checkpoint_path = f"{PATH}/{LOG_FILE.replace('.log','checkpoint_pytorch.pth')}"
+
+#training loop
+for epoch in range(EPOCHS):
+    train_loss = train_one_epoch(model, train_loader, optimizer, criterion, device)
+    valid_loss, valid_mae, valid_var= validate(model, val_loader, criterion, device)
+    
+    print(f"Epoch {epoch+1}/{EPOCHS} Train Loss: {train_loss:.4f}, Validation Loss: {valid_loss:.4f}, Validation MAE: {valid_mae:.4f} Pred/targets variance: {valid_var:.4f}")
+    
+    #early stopping
+    if valid_loss < best_valid_loss - DELTA:
+        best_valid_loss = valid_loss
+        patience_counter = 0
+        torch.save(model.state_dict(), checkpoint_path)  #save new best model
+        print(f"Model saved to {checkpoint_path}")
+    else:
+        patience_counter += 1
+        print(f"{patience_counter}/{early_stopping_patience} epochs without improvement")
+        if patience_counter >= early_stopping_patience:
+            print("Early stopping triggered!")
+            break
+
+#test 
+
+print(f'Final evaluation based on separate test set from {TEST_DIR_NAME} directory')
+print(f"Number of test samples: {len(test_loader.dataset)}")
+test_loss, test_mae, test_var= validate(model, test_loader, criterion, device)
+print(f"Test MAE: {test_mae:.4f} Pred/targets variance: {test_var:.4f}")
--- scripts/dataloaders.py
+++ scripts/dataloaders.py
@@ -6,6 +6,7 @@ import torch
 from torchvision.datasets import ImageFolder
 from torchvision import transforms
 from PIL import Image
+from sklearn.model_selection import train_test_split
 
 from .reax_ff_data import bo
 from scripts.making_df import *
@@ -40,12 +41,13 @@ def read_files():
     return files
   
 class CustomDataset(torch.utils.data.Dataset):
-    def __init__(self, transform=None):
+    def __init__(self, file_list = None, data_dir = TRAIN_DIR_NAME, transform=None):
         self.transform = transform
         name = f'{DB}.csv'
-        self.df = pd.read_csv(os.path.join(PATH, name))
-        self.files = os.listdir(os.path.join(PATH, TRAIN_DIR_NAME)) + os.listdir(os.path.join(PATH, TEST_DIR_NAME))
+        self.df = pd.read_csv(os.path.join(PATH, name)) 
+        self.files = os.listdir(os.path.join(PATH, data_dir)) if file_list is None else file_list
         self.labels = {row['ID']: row[PREDICTED_VALUE] for _, row in self.df.iterrows()}
+        self.data_dir = data_dir
         
     def __len__(self):
         return len(self.files)#, len(self.df)
@@ -53,22 +55,14 @@ class CustomDataset(torch.utils.data.Dataset):
     def __getitem__(self, idx):
         #img_name = f"{self.df['ID'].iloc[idx]}.png"
         img_name = f"{self.files[idx]}"
-        try: 
-            image = Image.open(f'{PATH}/{TRAIN_DIR_NAME}/{img_name}').convert('RGB') 
-
-        except FileNotFoundError:
-            image = Image.open(f'{PATH}/{TEST_DIR_NAME}/{img_name}').convert('RGB')
-
-        label = self.df[PREDICTED_VALUE].iloc[idx]
-        label = torch.tensor(label, dtype=torch.float32)#.to(DEVICE)
+        image = Image.open(f'{PATH}/{self.data_dir}/{img_name}').convert('RGB') 
 
         img_id = img_name.split('.')[0]  
-        print(len(self.files), idx, img_id)
+        #print(len(self.files), idx, img_id)
 
         label = self.labels.get(img_id, -1)
-        label = torch.tensor(label, dtype=torch.float32)#.to(DEVICE)
+        label = torch.tensor(label, dtype=torch.float32)
 
-        
         if self.transform:
             image = self.transform(image)
 
@@ -82,14 +76,22 @@ def dataloader_conv(n = 0):
     else: 
         print(f'Program did not create new images because DELETE parameter is set to False')
 
-    train_transforms = transforms.Compose([transforms.Resize((RESIZE,RESIZE)), transforms.ToTensor()])
-    train_dataset = CustomDataset(transform=train_transforms)
+    trans = transforms.Compose([transforms.Resize((RESIZE,RESIZE)), transforms.ToTensor()])
+
+    train_files, val_files = train_test_split(os.listdir(f"{PATH}/{TRAIN_DIR_NAME}"), test_size=1-TRAIN_VAL_SPLIT, random_state=42)
+
+    #create datasets (based on TRAIN_DIR_NAME)
+    train_dataset = CustomDataset(transform=trans, data_dir=TRAIN_DIR_NAME, file_list=train_files)
+    val_dataset = CustomDataset(transform=trans, data_dir=TRAIN_DIR_NAME, file_list=val_files)
+
+    #create dataloaders
     train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
+    val_loader = DataLoader(val_dataset, batch_size=BATCH_SIZE, shuffle=False)
 
-    val_dataset = CustomDataset(transform=train_transforms)
-    val_loader = DataLoader(val_dataset, batch_size=BATCH_SIZE, shuffle=True)
+    #test dataset (based on TEST_DIR_NAME)
+    test_dataset = CustomDataset(transform=trans, data_dir=TEST_DIR_NAME)
+    test_loader = DataLoader(test_dataset, batch_size=int(len(os.listdir(os.path.join(PATH, TEST_DIR_NAME)))/CYCLE), shuffle=False)
 
-
-    return train_loader, val_loader, raw_data
+    return train_loader, val_loader, test_loader, raw_data
 
 
